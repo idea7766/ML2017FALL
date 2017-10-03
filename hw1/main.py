e0fe3dfeabf3622089ec07051bf48af96b7b1a11
ke <idea7766@gmail.com>
2017-10-03 19:02:24 +0800

    10 / 3, 要繼續改utils.load()

--- hw1/main.py
+++ hw1/main.py
@@ -12,9 +12,6 @@ output_path = sys.argv[3]
 
 arr_train = utils.load(train_path, mode = 'train')
 arr_test = utils.load(test_path, mode = 'test')
-print(arr_train.shape)
-print(arr_test.shape)
-
 
 #製作 "x, y" ，以每個小時預測下一個小時
 
@@ -23,22 +20,12 @@ y = arr_train[1:,9]
 print(x.shape)
 print(y.shape)
 
-# b, w = lm.LinearRegression(x, y, lr = 0.01, epoch = 100000)
-b, w = lm.LinearRegression_close(x, y)
+b, w = lm.LinearRegression(x, y, lr = 0.01, epoch = 100000)
+# b, w = lm.LinearRegression_close(x, y)
 
 x_test = utils.scaling(arr_test, max, min) 
 predicted = lm.predcit(x_test, b, w)
 
 utils.write_out_ans(predicted, output_path)
 print(predicted)
-'''
-fig, ax =plt.subplots()
-#點座標
-ax.scatter(y, predicted)
-#劃線
-ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw = 4)
-#欄位名稱
-ax.set_xlabel('Measured')
-ax.set_ylabel('Predicted')
-plt.show()
-'''
+
--- hw1/utils.py
+++ hw1/utils.py
@@ -22,7 +22,7 @@ def scaling(data, max, min):
         new_data = (data - min) / (max - min)
     return new_data
 
-def load(path, mode = 'train'): # only for ML2017FALL hw1
+def load(path, mode = 'train', fea_select = None): # only for ML2017FALL hw1
     if mode == 'train':
         df_pm25 = pd.read_csv(path, encoding='big5')
         #將 'NR' 改成 0
@@ -30,11 +30,18 @@ def load(path, mode = 'train'): # only for ML2017FALL hw1
         arr_pm25_pre = arr_pm25_pre.astype(float)
 
         arr_pm25 = np.array([arr_pm25_pre[:18, 0]])
-        for i in range(240):
-            for j in range(24):
+
+        day_num = int(arr_pm25[0] / 18)
+        hour_of_day = 24
+        for i in range(day_num):
+            for j in range(hour_of_day):
                 if i==0 and j == 0:
                     continue
-                arr_pm25 = np.append(arr_pm25, [arr_pm25_pre[18*i:18+18*i, j]], axis = 0)
+                data_of_hour = arr_pm25_pre[18*i:18+18*i, j]
+                arr_pm25 = np.append(arr_pm25, [data_of_hour], axis = 0)
+        
+        if fea_select != None:
+            arr_pm25 = arr[:, fea_select]
         arr_pm25 = align(arr_pm25, 9)
         return arr_pm25
 
@@ -43,21 +50,21 @@ def load(path, mode = 'train'): # only for ML2017FALL hw1
         #將 'NR' 改成 0
         arr_pm25_pre = df_pm25.iloc[:,2:].replace('NR', 0).values
         arr_pm25_pre = arr_pm25_pre.astype(float)
-        # print(arr_pm25_pre.shape[0])
         arr_pm25 = np.array([np.transpose(arr_pm25_pre[:18]).flatten()])
 
-        run = int(arr_pm25_pre.shape[0]/18)
-        # print(run)
+        data_num = int(arr_pm25_pre.shape[0]/18)
         for i in range(run):
             if i == 0:
                 continue
-            arr_pm25 = np.append(arr_pm25, [np.transpose(arr_pm25_pre[18*i:18*i+18]).flatten()], axis=0)
+            data_of_9hr = arr_pm25, [np.transpose(arr_pm25_pre[18*i:18*i+18]
+            arr_pm25 = np.append(data_of_9hr).flatten()], axis=0)
 
         return arr_pm25
 
 def align(data, num):
     if num <= 0 or None:
         raise('請輸入大於 0 的數值')
+
     run = data.shape[0] - num + 1 # 毎 n 比當成 1 筆 data
     ls_align_data = np.array([data[0:num].flatten()])
     for ind in range(run):
